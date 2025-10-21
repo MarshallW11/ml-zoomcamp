17e9e2db10f7fa67dc2227ef97ace596b7c07516
Marshall <58564958+MarshallW11@users.noreply.github.com>
2025-10-21 16:19:06 +0000

    FastAPI code

--- 05-Deploying ML Models/predict.py
+++ 05-Deploying ML Models/predict.py
@@ -1,52 +1,27 @@
-import pandas as pd
-import numpy as np
-import sklearn
+
 import pickle
+import uvicorn
+from fastapi import FastAPI
+from sklearn.pipeline import make_pipeline
 
-
-# In[34]:
-
-
-print(f'pandas=={pd.__version__}')
-print(f'numpy=={np.__version__}')
-print(f'sklearn=={sklearn.__version__}')
-
-
-# In[35]:
-
-
-from sklearn.feature_extraction import DictVectorizer
-from sklearn.linear_model import LogisticRegression
-
-customer = {
-    'gender': 'male',
-    'seniorcitizen': 0,
-    'partner': 'no',
-    'dependents': 'yes',
-    'phoneservice': 'no',
-    'multiplelines': 'no_phone_service',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'no',
-    'onlinebackup': 'yes',
-    'deviceprotection': 'no',
-    'techsupport': 'no',
-    'streamingtv': 'no',
-    'streamingmovies': 'no',
-    'contract': 'month-to-month',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'electronic_check',
-    'tenure': 6,
-    'monthlycharges': 29.85,
-    'totalcharges': 129.85
-}
+app = FastAPI(title="Churn Prediction")
 
 with open('model.bin', 'rb') as f_in:
     pipeline = pickle.load(f_in)
 
+def predict_single(customer):
+   result = pipeline.predict_proba(customer)[0, 1]
+   return float(result)
 
-churn = pipeline.predict_proba(customer)[0, 1]
+@app.post("/predict")
+def predict(customer):
+    churn = predict_single(customer)
+    
+    return {
+        
+        "churn_probability": prob,
+        "churn_decision": bool(churn >= 0.5)
+    }
 
-if churn >= 0.5:
-    print(f'Send promotional email {churn}')
-else:
-    print(f"Don't send promotional email {1 - churn}")
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9696)
